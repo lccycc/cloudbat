8bea4c79d31300d8750372ff9478a5c91e0ff8e9
lcc <lcc@lccf>
2013-08-16 22:10:29 +0800

    cd

---
+++ benchmark/cd.speccmds.cmd
@@ -0,0 +1,60 @@
+perlbench
+/localdisk/yechen/cpu2006/benchspec/CPU2006/400.perlbench/run/run_base_ref_domU.0000/
+./perlbench_base.domU -I./lib checkspam.pl 2500 5 25 11 150 1 1 1 1
+bzip2
+/localdisk/yechen/cpu2006/benchspec/CPU2006/401.bzip2/run/run_base_ref_domU.0000/
+./bzip2_base.domU input.source 280
+gcc
+/localdisk/yechen/cpu2006/benchspec/CPU2006/403.gcc/run/run_base_ref_domU.0000/
+./gcc_base.domU 166.i -o 166.s
+bwaves
+/localdisk/yechen/cpu2006/benchspec/CPU2006/410.bwaves/run/run_base_ref_domU.0000/
+./bwaves_base.domU
+mcf
+/localdisk/yechen/cpu2006/benchspec/CPU2006/429.mcf/run/run_base_ref_domU.0000/
+./mcf_base.domU inp.in
+zeusmp
+/localdisk/yechen/cpu2006/benchspec/CPU2006/434.zeusmp/run/run_base_ref_domU.0000/
+./zeusmp_base.domU
+namd
+/localdisk/yechen/cpu2006/benchspec/CPU2006/444.namd/run/run_base_ref_domU.0000/
+./namd_base.domU --input namd.input --iterations 38 --output namd.out 
+dealII
+/localdisk/yechen/cpu2006/benchspec/CPU2006/447.dealII/run/run_base_ref_domU.0000/
+./dealII_base.domU 23
+soplex
+/localdisk/yechen/cpu2006/benchspec/CPU2006/450.soplex/run/run_base_ref_domU.0000/
+./soplex_base.domU -s1 -e -m45000 pds-50.mps
+povray
+/localdisk/yechen/cpu2006/benchspec/CPU2006/453.povray/run/run_base_ref_domU.0000/
+./povray_base.domU SPEC-benchmark-ref.ini
+hmmer
+/localdisk/yechen/cpu2006/benchspec/CPU2006/456.hmmer/run/run_base_ref_domU.0000/
+./hmmer_base.domU nph3.hmm swiss41
+sjeng
+/localdisk/yechen/cpu2006/benchspec/CPU2006/458.sjeng/run/run_base_ref_domU.0000/
+./sjeng_base.domU ref.txt
+libquantum
+/localdisk/yechen/cpu2006/benchspec/CPU2006/462.libquantum/run/run_base_ref_domU.0000/
+./libquantum_base.domU 1397 8
+h264ref
+/localdisk/yechen/cpu2006/benchspec/CPU2006/464.h264ref/run/run_base_ref_domU.0000/
+./h264ref_base.domU -d foreman_ref_encoder_baseline.cfg
+tonto
+/localdisk/yechen/cpu2006/benchspec/CPU2006/465.tonto/run/run_base_ref_domU.0000/
+./tonto_base.domU
+lbm
+/localdisk/yechen/cpu2006/benchspec/CPU2006/470.lbm/run/run_base_ref_domU.0000/
+./lbm_base.domU 3000 reference.dat 0 0 100_100_130_ldc.of
+omnetpp
+/localdisk/yechen/cpu2006/benchspec/CPU2006/471.omnetpp/run/run_base_ref_domU.0000/
+./omnetpp_base.domU omnetpp.ini
+astar
+/localdisk/yechen/cpu2006/benchspec/CPU2006/473.astar/run/run_base_ref_domU.0000/
+./astar_base.domU BigLakes2048.cfg
+wrf
+/localdisk/yechen/cpu2006/benchspec/CPU2006/481.wrf/run/run_base_ref_domU.0000/
+./wrf_base.domU
+sphinx3
+/localdisk/yechen/cpu2006/benchspec/CPU2006/482.sphinx3/run/run_base_ref_domU.0000/
+./sphinx_livepretend_base.domU ctlfile . args.an4
